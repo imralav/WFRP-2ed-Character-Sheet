ffa163b1826bfd4bd22fe0449182134fd1fa93b4
Tomasz Karczewski <imralav@gmail.com>
2018-07-19 14:02:01 +0200

    #24 added tests for OpenCharacter

---
+++ frontend/src/open-character/OpenCharacter.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import OpenCharacter from '../open-character';
+import TestUtils from '../utils/TestUtils';
+
+describe('OpenCharacter', () => {
+    const CHARACTER_DATA = {player: {name: 'someName'}};
+    const CHARACTER_DATA_JSON = JSON.stringify(CHARACTER_DATA);
+    const ENCODED_CHARACTER_DATA = btoa(CHARACTER_DATA_JSON);
+    let component;
+    let componentRoot;
+    let tree;
+    beforeEach(() => {
+        component = TestUtils.createComponentWithDefaultIntlAndRouter(
+            <OpenCharacter/>
+        );
+        componentRoot = component.root;
+        tree = component.toJSON();
+    });
+    it('should match previous snapshot', () => {
+        expect(tree).toMatchSnapshot();
+    });
+    describe('character data change', () => {
+        const event = {
+            target: {
+                value: ENCODED_CHARACTER_DATA
+            }
+        };
+        beforeEach(() => {
+            componentRoot.findByType('textarea').props.onChange(event);
+        });
+        it('should add unparsed character data to state', () => {
+            expect(componentRoot.findByType(OpenCharacter).instance.state.unparsedCharacterData).toEqual(ENCODED_CHARACTER_DATA);
+        });
+        describe('character data load', () => {
+            beforeEach(() => {
+                componentRoot.findByType('button').props.onClick();
+            });
+            it('should parse character data and set it on state', () => {
+                expect(componentRoot.findByType(OpenCharacter).instance.state.characterData).toEqual(CHARACTER_DATA);
+            });
+            it('should match snapshot', () => {
+                expect(tree).toMatchSnapshot();
+            });
+        });
+    });
+});
